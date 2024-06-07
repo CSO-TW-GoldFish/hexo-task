2101dbef8263beff47c4fb1b8cda19a42355dba5
CSO-TW-GoldFish <hey_fann@yahoo.com>
2024-06-07 18:12:22 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.btn').click(function(event) {
+      $('.btn').toggleClass("active");
+      $('.hamburger').toggleClass("close");
+      $('.closure').toggleClass("close");
+    });
+  });
+  
